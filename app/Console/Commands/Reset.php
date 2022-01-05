946266211a85f76640a58842125dbfbd9f5b2117
ScriptMint <hello@scriptmintsolution.com>
2022-01-05 12:51:15 +0530

    Demo related changes & reset command

---
+++ app/Console/Commands/Reset.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Reset extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \Artisan::call('migrate:rollback --force');
+        \Artisan::call('migrate --force');
+        \Artisan::call('db:seed --force');
+    }
+}
--- resources/js/views/Layouts/App.vue
+++ resources/js/views/Layouts/App.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="flex flex-col bg-gray-50 h-screen">
-        <div></div>
+        <div class=" bg-indigo-900 text-center py-4 lg:px-4">
+            <a href="https://ui.scriptmint.com">
+                <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+                        <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
+                        <span class="font-semibold mr-2 text-left flex-auto">Looking for Laravel, VueJS, Tailwind Admin Panel? - Check Super Advanced MintUI Admin Panel Live Demo! </span>
+                        <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+
+                </div>
+            </a>
+        </div>
         <div class="flex-1 overflow-y-auto">
             <div class="px-6 pt-10 flex justify-between">
                 <div class="flex items-center cursor-pointer" @click="router.push({name: 'Dashboard'})">
@@ -12,6 +21,11 @@
 
                 <div class="flex items-center space-x-4">
                     <a href="https://github.com/scriptmintsolution/laravel-vue3-tailwind-crud"><i class="fab fa-github fa-2x"></i></a>
+                    <a href="https://ui.scriptmint.com">
+                        <BaseButton>
+                            MintUI Admin Live Demo
+                        </BaseButton>
+                    </a>
                 </div>
             </div>
 
