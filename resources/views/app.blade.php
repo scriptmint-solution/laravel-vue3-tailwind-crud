919e30121e55c754927902d6e1857bf10231f392
ScriptMint <hello@scriptmintsolution.com>
2022-01-07 20:27:25 +0530

    Google analytics tracking

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,5 +18,13 @@
             <router-view></router-view>
         </div>
         <script src="{{ mix('/js/app.js') }}"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{env('GA_TRACKING_ID')}}"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', {{env('GA_TRACKING_ID')}});
+        </script>
     </body>
 </html>
