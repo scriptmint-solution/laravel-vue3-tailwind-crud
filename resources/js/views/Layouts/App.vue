871a37e6c5ecb8466a3b7f12f5f11113d76d5ce3
ScriptMint <hello@scriptmintsolution.com>
2022-01-30 19:00:34 +0530

    Update App.vue

--- resources/js/views/Layouts/App.vue
+++ resources/js/views/Layouts/App.vue
@@ -5,7 +5,7 @@
             <a href="https://ui.scriptmint.com" target="_blank">
                 <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
                         <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
-                        <span class="font-semibold mr-2 text-left flex-auto">Looking for Laravel, VueJS, Tailwind Admin Panel? - Check Super Advanced MintUI Admin Panel Live Demo! </span>
+                        <span class="font-semibold mr-2 text-left flex-auto">Looking for Laravel, VueJS, Tailwind Admin Panel? - Check Super Advanced Vana Admin Panel Live Demo! </span>
                         <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
 
                 </div>
@@ -24,7 +24,7 @@
                     <a href="https://github.com/scriptmintsolution/laravel-vue3-tailwind-crud" target="_blank"><i class="fab fa-github fa-2x"></i></a>
                     <a href="https://ui.scriptmint.com" target="_blank">
                         <BaseButton>
-                            MintUI Admin Live Demo
+                            Vana Admin Live Demo
                         </BaseButton>
                     </a>
                 </div>
